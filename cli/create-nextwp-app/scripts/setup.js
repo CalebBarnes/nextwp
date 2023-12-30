1b2932af0f72bb8562eb83049aacd9b7a0e4900e
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-12-29 17:28:02 -0800

    add console logs

--- cli/create-nextwp-app/scripts/setup.js
+++ cli/create-nextwp-app/scripts/setup.js
@@ -6,16 +6,25 @@ let destFile;
 
 switch (os.platform()) {
   case "darwin":
+    console.log("Copying binary for", os.platform());
     sourceFile = "./dist/create-nextwp-app-mac";
+    console.log("sourceFile: ", sourceFile);
     destFile = "./bin/create-nextwp-app";
+    console.log("destFile: ", destFile);
     break;
   case "linux":
+    console.log("Copying binary for", os.platform());
     sourceFile = "./dist/create-nextwp-app-linux";
+    console.log("sourceFile: ", sourceFile);
     destFile = "./bin/create-nextwp-app";
+    console.log("destFile: ", destFile);
     break;
   case "win32":
+    console.log("Copying binary for", os.platform());
     sourceFile = "./dist/create-nextwp-app.exe";
+    console.log("sourceFile: ", sourceFile);
     destFile = "./bin/create-nextwp-app.exe";
+    console.log("destFile: ", destFile);
     break;
   default:
     console.error("Unsupported platform:", os.platform());
