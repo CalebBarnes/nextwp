1d425e19e5734d675060f47af1ea0d09bf14b0aa
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-12-29 17:36:55 -0800

    log errors in setup script

--- cli/create-nextwp-app/scripts/setup.js
+++ cli/create-nextwp-app/scripts/setup.js
@@ -1,35 +1,41 @@
 const os = require("os");
 const fs = require("fs");
 
-let sourceFile;
-let destFile;
+try {
+  let sourceFile;
+  let destFile;
 
-switch (os.platform()) {
-  case "darwin":
-    console.log("Copying binary for", os.platform());
-    sourceFile = "./dist/create-nextwp-app-mac";
-    console.log("sourceFile: ", sourceFile);
-    destFile = "./bin/create-nextwp-app";
-    console.log("destFile: ", destFile);
-    break;
-  case "linux":
-    console.log("Copying binary for", os.platform());
-    sourceFile = "./dist/create-nextwp-app-linux";
-    console.log("sourceFile: ", sourceFile);
-    destFile = "./bin/create-nextwp-app";
-    console.log("destFile: ", destFile);
-    break;
-  case "win32":
-    console.log("Copying binary for", os.platform());
-    sourceFile = "./dist/create-nextwp-app.exe";
-    console.log("sourceFile: ", sourceFile);
-    destFile = "./bin/create-nextwp-app.exe";
-    console.log("destFile: ", destFile);
-    break;
-  default:
-    console.error("Unsupported platform:", os.platform());
-    process.exit(1);
+  console.log("os Platform: ", os.platform());
+
+  switch (os.platform()) {
+    case "darwin":
+      sourceFile = "./dist/create-nextwp-app-mac";
+      destFile = "./bin/create-nextwp-app";
+      break;
+    case "linux":
+      sourceFile = "./dist/create-nextwp-app-linux";
+      destFile = "./bin/create-nextwp-app";
+
+      break;
+    case "win32":
+      sourceFile = "./dist/create-nextwp-app.exe";
+      destFile = "./bin/create-nextwp-app.exe";
+
+      break;
+    default:
+      console.error("Unsupported platform:", os.platform());
+      process.exit(1);
+  }
+
+  console.log("Copying binary for", os.platform());
+  console.log("sourceFile: ", sourceFile);
+  console.log("destFile: ", destFile);
+
+  fs.copyFileSync(sourceFile, destFile);
+  console.log("Copied binary for", os.platform());
+  fs.chmodSync(destFile, "755");
+  console.log("Changed permissions for", os.platform());
+} catch (error) {
+  console.error("Error:", error);
+  process.exit(1);
 }
-
-fs.copyFileSync(sourceFile, destFile);
-fs.chmodSync(destFile, "755");
