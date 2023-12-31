5c9c1204fbd9fdeceeb2e537b9fe4fe313784f14
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-12-30 20:07:52 -0800

    streamline cli - remove postinstall script
    directly execute correct binary based on os platform instead of moving stuff around in a postinstall script

--- packages/create-nextwp-app/bin/create-nextwp-app.js
+++ packages/create-nextwp-app/bin/create-nextwp-app.js
@@ -2,11 +2,28 @@
 const { spawn } = require("child_process");
 const path = require("path");
 
-const executable =
-  process.platform === "win32" ? "create-nextwp-app.exe" : "create-nextwp-app";
+const executable = getExecutable();
 const executablePath = path.join(__dirname, executable);
 
 const child = spawn(executablePath, process.argv.slice(2), {
   stdio: "inherit",
 });
 child.on("exit", process.exit);
+
+function getExecutable() {
+  const osPlatform = process.platform;
+  let executable = "create-nextwp-app";
+
+  switch (osPlatform) {
+    case "win32":
+      executable = "create-nextwp-app.exe";
+    case "darwin":
+      executable = "create-nextwp-app";
+    case "linux":
+      executable = "create-nextwp-app-linux";
+    default:
+      executable = "create-nextwp-app";
+  }
+
+  return `../dist/${executable}`;
+}
