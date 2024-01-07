b7c5bc4024fc322856016509b55afe0b632417d5
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-01-06 20:59:37 -0800

    docs download route handler - only cache no store for first releases request

--- apps/docs/src/app/api/download/[package]/route.ts
+++ apps/docs/src/app/api/download/[package]/route.ts
@@ -60,7 +60,6 @@ const fetchOptions = {
   headers: {
     accept: 'application/vnd.github+json',
     Authorization: `Bearer ${process.env.GITHUB_PAT}`,
-    cache: 'no-store',
   },
 }
 
@@ -78,7 +77,7 @@ export async function GET(
 
   const releasesRes = await fetch(
     `https://api.github.com/repos/CalebBarnes/nextwp/releases?per_page=1`,
-    fetchOptions,
+    { ...fetchOptions, cache: 'no-store' },
   )
   const releases = (await releasesRes.json()) as Release[]
 
