4128e96778c30b016b1131c3d745a80c542064ce
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-01-12 19:11:52 -0800

    adding post type archives

--- packages/core/src/next-app-functions/generate-static-params.ts
+++ packages/core/src/next-app-functions/generate-static-params.ts
@@ -1,4 +1,7 @@
 import { getAllItems } from "../api/get-all-items";
+import { getPostTypes } from "../api/get-post-types";
+import { getTaxonomies } from "../api/get-taxonomies";
+import { debug } from "../utils/debug-log";
 
 /**
  * This function is used to statically generate routes at build time instead of on-demand at request time.
@@ -15,7 +18,7 @@ import { getAllItems } from "../api/get-all-items";
  */
 export async function generateStaticParams({
   wpUrl = process.env.NEXT_PUBLIC_WP_URL || "",
-  postTypes = ["pages", "posts"],
+  postTypes = ["pages", "posts"], // taxonomies = ["category", "post_tag"],
 }: {
   /**
    * The URL of the WP SITE to fetch data from the REST API.
@@ -25,6 +28,11 @@ export async function generateStaticParams({
    * The post types to include in the static generation.
    */
   postTypes?: string[];
+
+  /**
+   * The taxonomies to include in the static generation.
+   */
+  // taxonomies?: string[];
 }) {
   if (!wpUrl) {
     throw new Error(
@@ -35,21 +43,72 @@ export async function generateStaticParams({
   const staticParams: { paths: string[] }[] = [];
   const allItems = await getAllItems(postTypes);
 
+  const wpPostTypes = await getPostTypes();
+  // console.log(wpPostTypes);
+
+  for (const postType of postTypes) {
+    const itemKey = Object.keys(wpPostTypes).find(
+      (key) => wpPostTypes[key].rest_base === postType
+    );
+
+    const matchingPostType = wpPostTypes[itemKey];
+
+    // console.log({ matchingPostType });
+
+    if (
+      matchingPostType.has_archive &&
+      typeof matchingPostType.has_archive === "string"
+    ) {
+      staticParams.push({
+        paths: [matchingPostType.has_archive],
+      });
+    }
+
+    // todo: fetch first page of this type to check how many pages there are
+    // todo: then generate static paths for each page
+    // todo: example: /blog/page/2 /blog/page/3 etc
+
+    // todo: check which taxonomies are registered for this post type
+    // todo: then fetch all terms for each taxonomy
+    // todo: then generate static paths for each term
+  }
+
+  // for (const key in wpPostTypes) {
+  //   const item = wpPostTypes.find(postType = postType.rest_base === key)
+
+  //   const item = wpPostTypes[key];
+  //   if (item.has_archive && typeof item.has_archive === "string") {
+  //     staticParams.push({
+  //       paths: [item.has_archive],
+  //     });
+  //   }
+  // }
+
+  // debug.log(staticParams);
+
+  // const wpTaxonomies = await getTaxonomies();
+
+  // for (const key in wpTaxonomies) {
+  //   if (!taxonomies.includes(key)) {
+  //     continue;
+  //   }
+
+  //   const item = wpTaxonomies[key];
+  //   const endpoint = `${process.env.NEXT_PUBLIC_WP_URL}/wp-json/wp/v2/${item.rest_base}`;
+  //   const req = await fetch(endpoint);
+  //   const data = await req.json();
+
+  //   for (const tax of data) {
+  //     const path = tax.link.replace(wpUrl, "");
+  //     const pathBreadcrumbs = path.split("/").filter((x) => x);
+
+  //     staticParams.push({
+  //       paths: [...pathBreadcrumbs],
+  //     });
+  //   }
+  // }
+
   for (const item of allItems) {
-    // if (item.path === "/") {
-    //   staticParams.push({
-    //     paths: ["/"],
-    //   });
-    //   continue;
-    // }
-
-    //       if (item.path === "/") {
-    //         staticParams.push({
-    //           paths: ["index"],
-    //         });
-    //         continue;
-    //       }
-
     const pathBreadcrumbs = item.path.split("/").filter((x) => x);
 
     staticParams.push({
@@ -59,12 +118,6 @@ export async function generateStaticParams({
 
   staticParams.push({
     paths: ["/"],
-  });
-  staticParams.push({
-    paths: ["index"],
-  });
-  staticParams.push({
-    paths: ["all-products"],
   });
 
   return staticParams;
