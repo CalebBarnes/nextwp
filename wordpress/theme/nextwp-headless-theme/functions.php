cfae7733a073534eb20673a2cb697ceb49bbbb38
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-01-22 14:31:17 -0800

    add theme support menus

--- wordpress/theme/nextwp-headless-theme/functions.php
+++ wordpress/theme/nextwp-headless-theme/functions.php
@@ -2,5 +2,6 @@
 include_once('inc/functions/register-required-plugins.php');
 include_once('inc/functions/required-plugins.php');
 
-add_theme_support( 'custom-logo' );
-add_theme_support( 'post-thumbnails' );
+add_theme_support('custom-logo');
+add_theme_support('post-thumbnails');
+add_theme_support('menus');
