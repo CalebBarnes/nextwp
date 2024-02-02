ee6ceaef7a4ecd4c2662e94acde965f1c5b00454
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-02-02 04:10:12 -0800

    add build script including js format file and version with dirty -dev

---
+++ packages/cli/build-dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export VERSION=$(git describe --tags --match "nextwp-cli-*" --always --dirty="-dev")
+mkdir -p tmp/js
+cp js/format.js dist/js/format.js
+go build -ldflags "-X main.version=$VERSION" -o ./tmp/main ./cmd/app
