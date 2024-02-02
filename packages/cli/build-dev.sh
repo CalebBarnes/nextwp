2c29ec468fb1305ac6928828e4ccc8b80a86c17e
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-02-02 05:03:09 -0800

    for dev with air, just copy the format.js over, no need to bundle in dev

--- packages/cli/build-dev.sh
+++ packages/cli/build-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export VERSION=$(git describe --tags --match "nextwp-cli-*" --always --dirty="-dev")
 mkdir -p tmp/js
-cp js/format.js dist/js/format.js
+cp js/format.js tmp/js/format.js
 go build -ldflags "-X main.version=$VERSION" -o ./tmp/main ./cmd/app
