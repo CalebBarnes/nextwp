8bcaa21d4f7d9256dd025a2d3587cbc7dc964d7a
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-02-14 13:51:58 -0800

    build.sh

--- packages/cli/build.sh
+++ packages/cli/build.sh
@@ -1,2 +1,2 @@
 VERSION=$(git describe --tags --match "nextwp-cli-*" --always --dirty="-dev")
-go build -ldflags "-X main.version=$VERSION" -o nextwp-cli .
+go build -ldflags "-X main.version=$VERSION" -o nextwp-cli ./cmd/app
