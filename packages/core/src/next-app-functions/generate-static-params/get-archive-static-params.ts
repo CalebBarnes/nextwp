ca90109a4dac79613771478c82a3c734f75b0eb5
mosesintech <mosesmaxcosme@gmail.com>
2024-03-22 11:40:42 -0500

    add types to getArchivePaginationInfo and getStaticParamsFrom PaginationInfo

--- packages/core/src/next-app-functions/generate-static-params/get-archive-static-params.ts
+++ packages/core/src/next-app-functions/generate-static-params/get-archive-static-params.ts
@@ -1,7 +1,19 @@
 import { getPostTypes } from "../../api/get-post-types";
 import { getSiteSettings } from "../../api/get-site-settings";
 
-export async function getArchiveStaticParams({ postTypes }) {
+interface GetArchivePaginationInfo {
+  postTypes: string[]
+}
+
+interface GetStaticParamsFromPaginationInfo {
+  archiveSlug: string;
+  paginationInfo: {
+    totalPages: number | undefined;
+    totalItems: number | undefined;
+} | null
+}
+
+export async function getArchiveStaticParams({ postTypes }: GetArchivePaginationInfo) {
   const wpPostTypes = await getPostTypes();
   const settings = await getSiteSettings();
   const staticParams: { paths: string[] }[] = [];
@@ -135,7 +147,7 @@ async function getArchivePaginationInfo({
   }
 }
 
-function getStaticParamsFromPaginationInfo({ archiveSlug, paginationInfo }) {
+function getStaticParamsFromPaginationInfo({ archiveSlug, paginationInfo }: GetStaticParamsFromPaginationInfo) {
   const staticParams = [];
 
   for (let i = 1; i <= paginationInfo.totalPages; i++) {
